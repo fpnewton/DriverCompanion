7a2f193320bad3f13ef6d95ffd85eb3d6d4e2ff2
fpnewton <fnewton3@gatech.edu>
2012-11-14 16:48:23 -0500

    Put everything in to classes.

--- gen/edu/gatech/espresso/driver/companion/R.java
+++ gen/edu/gatech/espresso/driver/companion/R.java
@@ -15,21 +15,27 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btn_listen=0x7f070000;
-        public static final int menu_settings=0x7f070001;
+        public static final int btn_listen=0x7f080000;
+        public static final int menu_settings=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int main_activity=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int btn_listen=0x7f040002;
-        public static final int menu_settings=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int btn_listen=0x7f050002;
+        public static final int log_tag=0x7f050003;
+        public static final int menu_settings=0x7f050001;
+        public static final int pref_category_general=0x7f050004;
+        public static final int speech_dialog_title=0x7f050005;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+    }
+    public static final class xml {
+        public static final int application_preferences=0x7f040000;
     }
 }
--- src/edu/gatech/espresso/driver/companion/MainActivity.java
+++ src/edu/gatech/espresso/driver/companion/MainActivity.java
@@ -2,170 +2,91 @@
 package edu.gatech.espresso.driver.companion;
 
 
-import java.util.ArrayList;
-import java.util.Locale;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends Activity implements OnInitListener
+public class MainActivity extends Activity
 {
-	private static final String	TAG								= "TextToSpeechDemo";
+	private Contacts			contacts;
+	private TTS					tts;
+	private SpeechRecognizer	sr;
 
-	private TextToSpeech		mTts;
-	private Button				mAgainButton;
-
-	private static final int	VOICE_RECOGNITION_REQUEST_CODE	= 1234;
-
-	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Initialize text-to-speech. This is an asynchronous operation.
-		// The OnInitListener (second argument) is called after initialization
-		// completes.
-		mTts = new TextToSpeech(this, this // TextToSpeech.OnInitListener
-		);
-
-		// The button is disabled in the layout.
-		// It will be enabled upon initialization of the TTS engine.
-		mAgainButton = (Button) findViewById(R.id.btn_listen);
-
-		mAgainButton.setOnClickListener(new View.OnClickListener()
-		{
-			public void onClick(View v)
-			{
-				startVoiceRecognitionActivity();
-			}
-		});
+		contacts = new Contacts(this);
+		tts = new TTS(this);
+		sr = new SpeechRecognizer(this);
 	}
 
-	@Override
 	public void onDestroy()
 	{
-		// Don't forget to shutdown!
-		if (mTts != null)
-		{
-			mTts.stop();
-			mTts.shutdown();
-		}
-
 		super.onDestroy();
+
+		tts.onDestroy();
 	}
 
-	// Implements TextToSpeech.OnInitListener.
-	public void onInit(int status)
+	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// status can be either TextToSpeech.SUCCESS or TextToSpeech.ERROR.
-		if (status == TextToSpeech.SUCCESS)
-		{
-			// Set preferred language to US english.
-			// Note that a language may not be available, and the result will
-			// indicate this.
-			int result = mTts.setLanguage(Locale.US);
-			// Try this someday for some interesting results.
-			// int result mTts.setLanguage(Locale.FRANCE);
-			if (result == TextToSpeech.LANG_MISSING_DATA
-					|| result == TextToSpeech.LANG_NOT_SUPPORTED)
-			{
-				// Lanuage data is missing or the language is not supported.
-				Log.e(TAG, "Language is not available.");
-			}
-			else
-			{
-				// Check the documentation for other possible result codes.
-				// For example, the language may be available for the locale,
-				// but not for the specified country and variant.
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity, menu);
 
-				// The TTS engine has been successfully initialized.
-				// Allow the user to press the button for the app to speak
-				// again.
-				mAgainButton.setEnabled(true);
-				// Greet the user.
-				mTts.speak("Hello!", TextToSpeech.QUEUE_FLUSH, null);
+		return true;
+	}
+
+	public boolean onPrepareOptionsMenu(Menu menu)
+	{
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		int selectionId = item.getItemId();
+
+		if (selectionId == R.id.menu_settings)
+		{
+			startActivity(new Intent(this, SettingsActivity.class));
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+
+	public void btnListenClicked(View v)
+	{
+		// TODO
+		sr.startRecognizer();
+	}
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		sr.onActivityResult(requestCode, resultCode, data);
+		super.onActivityResult(requestCode, resultCode, data);
+
+		String results[] = sr.getResults();
+
+		if (results != null)
+		{
+			String split[] = results[0].split(" ");
+			
+			for (String s : split)
+			{
+				Log.i(getString(R.string.log_tag), contacts.getContact(s));
+				//tts.speak("I Heard: " + s);
+				//Phone.call(contacts.getContact(s), this);
 			}
 		}
 		else
 		{
-			// Initialization failed.
-			Log.e(TAG, "Could not initialize TextToSpeech.");
+			Log.e(getString(R.string.log_tag), "No speech results received.");
 		}
-	}
-
-	/**
-	 * Fire an intent to start the speech recognition activity.
-	 */
-	private void startVoiceRecognitionActivity()
-	{
-		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-		// Specify the calling package to identify your application
-		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
-
-		// Display an hint to the user about what he should say.
-		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
-
-		// Given an hint to the recognizer about what the user is going to say
-		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-				RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-		// Specify how many results you want to receive. The results will be
-		// sorted
-		// where the first result is the one with higher confidence.
-		intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
-
-		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
-	}
-
-	/**
-	 * Handle the results from the recognition activity.
-	 */
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
-		if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK)
-		{
-			// Fill the list view with the strings the recognizer thought it
-			// could have heard
-			ArrayList<String> matches = data
-					.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-			try
-			{
-				Thread.sleep(2000);
-			}
-			catch (InterruptedException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			Log.i(TAG, matches.toString());
-			mTts.speak("You said: " + matches.get(0), TextToSpeech.QUEUE_FLUSH, null);
-		}
-		
-		Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
-		while (phones.moveToNext())
-		{
-		  String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-		  String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-		  Log.i(TAG, name);
-		  Log.i(TAG, phoneNumber);
-		}
-		
-		phones.close();
-		super.onActivityResult(requestCode, resultCode, data);
 	}
 }
